8ca159350e74d2a8f5b2eefb2b45801e6c773735
Sylvain Faucherand <syl.faucherand@gmail.com>
2014-12-09 02:10:55 +0100

    [task] Simplify gruntfile tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,13 @@
 var grunt = require('grunt');
 
 grunt.loadNpmTasks('grunt-contrib-jshint');
-grunt.loadNpmTasks('grunt-istanbul-coverage');
-grunt.loadNpmTasks('grunt-exec');
 
 grunt.initConfig({
 
   jshint: {
     all : ['src/**/*.js', 'test/*.js', 'bin/**/*.js']
-  },
-
-  exec : {
-    coverage : {
-      cmd : 'npm run-script istanbul',
-      stdout : false,
-      stderr: false
-    }
-  },
-
-  coverage: {
-    options: {
-      thresholds: {
-        'statements': 90,
-        'branches': 90,
-        'lines': 90,
-        'functions': 90
-      },
-      dir: '',
-      root: 'coverage'
-    }
-  },
+  }
 
 });
 
-
-grunt.registerTask('validate', ['jshint:all', 'exec:coverage', 'coverage']);
+grunt.registerTask('default', ['jshint:all']);
